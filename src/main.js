2f383c614762c61343e643dc30cb3c01407d219b
Pavel Ulianov <paveluv@mail.ru>
2020-11-18 12:07:56 +0300

    Add new js file events-list.js. Refactor main.js, move main const to up, add new import function createEventsListElement, change location of call to rendering new and edit blocks. Task according first comments of mentor.

--- src/main.js
+++ src/main.js
@@ -3,21 +3,22 @@ import {createTripCostTemplate} from "./view/trip-cost.js";
 import {createSiteMenuTemplate} from "./view/site-menu.js";
 import {createSiteFilterTemplate} from "./view/site-filter.js";
 import {createEventsSortTemplate} from "./view/events-sort.js";
+import {createEventsListTemplate} from "./view/events-list.js";
 import {createEventsItemTemplate} from "./view/events-item.js";
 import {createEventsEditorTemplate} from "./view/events-edit.js";
-import {createEventsNewAddTemplate} from "./view/events-add-new.js";
-
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
+import {createEventsNewTemplate} from "./view/events-new.js";
 
 const siteHeaderElement = document.querySelector(`.page-header`);
 const siteMainElement = document.querySelector(`.page-main`);
 const siteTripMainElement = siteHeaderElement.querySelector(`.trip-main`);
-const siteControlHeaders = siteTripMainElement.querySelectorAll(`h2`);
+const [siteMenuHeader, siteFilterHeader] = siteTripMainElement.querySelectorAll(`h2`);
 const siteEventsElement = siteMainElement.querySelector(`.trip-events`);
 const siteEventsHeader = siteEventsElement.querySelector(`h2`);
 const EVENT_COUNT = 3;
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
 
 render(siteTripMainElement, createTripInfoTemplate(), `afterbegin`);
 
@@ -25,21 +26,19 @@ const siteTripInfoElement = siteTripMainElement.querySelector(`.trip-info`);
 
 render(siteTripInfoElement, createTripCostTemplate(), `beforeend`);
 
-render(siteControlHeaders[0], createSiteMenuTemplate(), `afterend`);
+render(siteMenuHeader, createSiteMenuTemplate(), `afterend`);
 
-render(siteControlHeaders[1], createSiteFilterTemplate(), `afterend`);
+render(siteFilterHeader, createSiteFilterTemplate(), `afterend`);
 
 render(siteEventsHeader, createEventsSortTemplate(), `afterend`);
 
-render(siteEventsElement, `<ul class="trip-events__list"></ul>`, `beforeend`);
+render(siteEventsElement, createEventsListTemplate(), `beforeend`);
 
 const listEventsElement = siteEventsElement.querySelector(`.trip-events__list`);
 
+render(listEventsElement, createEventsNewTemplate(), `afterbegin`);
+
 render(listEventsElement, createEventsEditorTemplate(), `afterbegin`);
-
-const eventsItemsElement = listEventsElement.querySelectorAll(`.trip-events__item`);
-
-render(eventsItemsElement[0], createEventsNewAddTemplate(), `afterend`);
 
 for (let i = 0; i < EVENT_COUNT; i++) {
   render(listEventsElement, createEventsItemTemplate(), `beforeend`);
--- src/view/events-new.js
+++ src/view/events-new.js
@@ -1,4 +1,4 @@
-export const createEventsNewAddTemplate = () => {
+export const createEventsNewTemplate = () => {
   return `<li class="trip-events__item">
               <form class="event event--edit" action="#" method="post">
                 <header class="event__header">
