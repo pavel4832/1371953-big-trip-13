45924042ff400384665cc717cd15eb8c7f24681d
Pavel Ulianov <paveluv@mail.ru>
2020-12-11 12:12:42 +0800

    add re-using function to event presenter

--- src/presenter/event.js
+++ src/presenter/event.js
@@ -1,6 +1,6 @@
 import EventView from "../view/events-item.js";
 import EventEditView from "../view/events-edit.js";
-import {render, replace, RenderPosition} from "../utils/render.js";
+import {render, replace, remove, RenderPosition} from "../utils/render.js";
 
 export default class Event {
   constructor(eventListContainer) {
@@ -17,6 +17,9 @@ export default class Event {
   init(event) {
     this._event = event;
 
+    const prevEventComponent = this._eventComponent;
+    const prevEventEditComponent = this._eventEditComponent;
+
     this._eventComponent = new EventView(this._event);
     this._eventEditComponent = new EventEditView(this._event);
 
@@ -24,9 +27,27 @@ export default class Event {
     this._eventEditComponent.setRollupClickHandler(this._handleFormSubmit);
     this._eventEditComponent.setFormSubmitHandler(this._handleFormSubmit);
 
-    render(this._eventListContainer, this._eventComponent, RenderPosition.BEFOREEND);
+    if (prevEventComponent === null || prevEventEditComponent === null) {
+      render(this._eventListContainer, this._eventComponent, RenderPosition.BEFOREEND);
+      return;
+    }
+
+    if (this._eventListContainer.getElement().contains(prevEventComponent.getElement())) {
+      replace(this._eventComponent, prevEventComponent);
+    }
+
+    if (this._eventListContainer.getElement().contains(prevEventEditComponent.getElement())) {
+      replace(this._eventEditComponent, prevEventEditComponent);
+    }
+
+    remove(prevEventComponent);
+    remove(prevEventEditComponent);
   }
 
+  destroy() {
+    remove(this._eventComponent);
+    remove(this._eventEditComponent);
+  }
   _replaceCardToForm() {
     replace(this._eventEditComponent, this._eventComponent);
     document.addEventListener(`keydown`, this._escKeyDownHandler);
