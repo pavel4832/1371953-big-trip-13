d49f3a9deed09bcf736d4f951bf62c438eb574d7
Pavel Ulianov <paveluv@mail.ru>
2020-12-25 15:05:20 +0800

    add filter presenter

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import SiteMenuView from "./view/site-menu.js";
-import SiteFilterView from "./view/site-filter.js";
 import {generateEvent} from "./mock/event.js";
 import TripPresenter from "./presenter/trip";
+import FilterPresenter from "./presenter/filter.js";
 import EventsModel from "./model/events.js";
 import FilterModel from "./model/filter.js";
 import {render, RenderPosition} from "./utils/render.js";
@@ -13,20 +13,6 @@ const siteTripMainElement = document.querySelector(`.trip-main`);
 const siteEventsElement = document.querySelector(`.trip-events`);
 const [siteMenuHeader, siteFilterHeader] = siteTripMainElement.querySelectorAll(`h2`);
 const events = new Array(EVENT_COUNT).fill().map(generateEvent).sort(sortEventDay);
-const filters = [
-  {
-    type: `everything`,
-    name: `Everything`
-  },
-  {
-    type: `future`,
-    name: `Future`
-  },
-  {
-    type: `past`,
-    name: `Past`
-  }
-];
 
 const eventsModel = new EventsModel();
 eventsModel.setEvents(events);
@@ -34,9 +20,9 @@ eventsModel.setEvents(events);
 const filterModel = new FilterModel();
 
 const tripPresenter = new TripPresenter(siteEventsElement, eventsModel);
+const filterPresenter = new FilterPresenter(siteFilterHeader, filterModel, eventsModel);
 
 render(siteMenuHeader, new SiteMenuView(), RenderPosition.AFTER);
 
-render(siteFilterHeader, new SiteFilterView(filters, `everything`), RenderPosition.AFTER);
-
+filterPresenter.init();
 tripPresenter.init();
